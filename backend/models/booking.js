4d2d04e06c34782765271075bfd84c6704826a09
Shan Pramuditha <shanpramu9474@gmail.com>
2025-08-15 11:28:30 +0530

    Extend booking schema with additional fields
    Added fields for adult and child count, guest address, total price, special notes, advance amount, and status to the booking schema. These changes enhance the model to support more detailed booking information.

--- backend/models/booking.js
+++ backend/models/booking.js
@@ -6,7 +6,16 @@ const bookingSchema = new mongoose.Schema({
   booked_room_no: { type: String, required: true },
   checkin_date: { type: Date, required: true },
   checkout_date: { type: Date, required: true },
-  phone_no: { type: String, required: true }
-}, { timestamps: true });
+  phone_no: { type: String, required: true },
+  adualt_count:{type: Number, required: true },
+  child_count: { type: Number, required: true },
+  guest_address: { type: String, required: true },
+  total_price: { type: Decimal128, required: true },
+  special_notes: { type: String, default: ''},
+  advance_amount: { type: Decimal128, default: '0' },
+  status: { type: String, default: 'Pending'},
+
+
+  }, { timestamps: true });
 
 module.exports = mongoose.model('Booking', bookingSchema);
