004150bce8e86d7a630952eac9ba09087aff6c02
yusri009 <197112727+yusri009@users.noreply.github.com>
2026-01-04 20:07:26 +0530

    fixed port number for cloud run

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // ✅ CORS setup: allow all origins, handle preflight
 app.use(cors({
