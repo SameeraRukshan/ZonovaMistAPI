d971e00490ae2833aceba209d68cd660d7e236dd
Kavindi Basnayaka <140165949+kavindi13@users.noreply.github.com>
2026-01-08 15:48:48 +0530

    Remove unnecessary blank line in authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,5 @@
 const jwt = require('jsonwebtoken');
 
-
 const authMiddleware = function (req, res, next) {
   const token = req.header('Authorization')?.replace('Bearer ', '');
   if (!token) return res.status(401).json({ message: 'No token, authorization denied' });
